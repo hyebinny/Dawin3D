83373c2af9d1d718d6225c65e7d821f037393213
hyebinny <hyebinkim@ewha.ac.kr>
2025-05-22 16:52:52 +0900

    edited model names

---
+++ Dawin3D_L/Dawin3D_L/Dawin3D_L/models/__init__.py
@@ -0,0 +1,10 @@
+"""
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+"""
+from Dawin3D_L.models.Dawin3D_L import Dawin3D_LUNet, Dawin3D_LEncoder
+
+__all__ = [
+    'Dawin3D_LUNet',
+    'Dawin3D_LEncoder',
+]
---
+++ Dawin3D_S/Dawin3D_S/Dawin3D_S/models/__init__.py
@@ -0,0 +1,10 @@
+"""
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+"""
+from Dawin3D_S.models.Dawin3D_S import Dawin3D_SUNet, Dawin3D_SEncoder
+
+__all__ = [
+    'Dawin3D_SUNet',
+    'Dawin3D_SEncoder',
+]
